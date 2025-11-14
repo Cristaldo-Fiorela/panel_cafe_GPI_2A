c132f4ff57a74e12b5059132672fe6f2e0425bf1
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2025-11-14 16:02:28 -0300

    get de usuario por ID

--- backend/src/routes/rutasUsuarios.js
+++ backend/src/routes/rutasUsuarios.js
@@ -24,5 +24,28 @@ router.get('/', (req, res) => {
   })
 })
 
+router.get('/:id', (req, res) => {
+  const ID = parseInt(req.params.id);
+  const SQL = `
+  SELECT u.id, u.username, u.email, u.id_rol, r.descripcion as rol 
+  FROM usuario as u
+  INNER JOIN rol as r ON u.id_rol = r.id_rol
+  WHERE u.id = ?;
+  `;
+
+  db.query(SQL, [ID], (err, result) => {
+    if(err) {
+      console.error('Error al obtene usuario:', err);
+      return res.status(500).json({ 
+        error: 'Error interno del servidor',
+        detalle: err.message 
+      });
+    }
+
+    if(result.length == 0) return res.status(404).json({ error: 'Usuario no encontrado' });
+    
+    res.json(result[0]);
+  })
+})
 
 module.exports = router; 
