9ffad35431fa4d56d3c1cbd89e80a500ca67ad77
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2025-11-16 16:12:02 -0300

    primer acercamiento a PUT de producto

--- backend/src/routes/rutasProductos.js
+++ backend/src/routes/rutasProductos.js
@@ -27,7 +27,7 @@ router.get('/:id', (req, res) => {
   const ID = parseInt(req.params.id);
   const SQL = `
   SELECT * from producto
-  WHERE id_producto = ?;
+  WHERE id_producto = ?
   `;
 
   db.query(SQL, [ID], (err, result) => {
@@ -93,6 +93,75 @@ router.post('/', async (req, res) => {
     });
   }
 });
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nombre, descripcion, precio, imagen_url, stock, disponible } = req.body;
+
+    // se desestructura el array que viene para tomar el 1er objeto encontrado e ignorar el 2do que son los metadatos
+    const [existe] = await db.query(
+      'SELECT * from producto WHERE id_producto = ?', [id]   
+    );
+
+    if(existe.length === 0) {
+      return res.status(404).json({
+        error: 'Producto no encontrado'
+      })
+    }
+
+    if (precio !== undefined && precio < 0) {
+      return res.status(400).json({ 
+        error: 'El precio no puede ser negativo' 
+      });
+    }
+
+    // query dinámico (solo actualiza lo que viene)
+    const campos = [];
+    const valores = [];
+    
+    if (nombre !== undefined) {
+      campos.push('nombre = ?');
+      valores.push(nombre);
+    }
+    if (descripcion !== undefined) {
+      campos.push('descripcion = ?');
+      valores.push(descripcion);
+    }
+    if (precio !== undefined) {
+      campos.push('precio = ?');
+      valores.push(precio);
+    }
+    if (imagen_url !== undefined) {
+      campos.push('imagen_url = ?');
+      valores.push(imagen_url);
+    }
+    if (stock !== undefined) {
+      campos.push('stock = ?');
+      valores.push(stock);
+    }
+    if (disponible !== undefined) {
+      campos.push('disponible = ?');
+      valores.push(disponible);
+    }
+
+    if (campos.length === 0) {
+      return res.status(400).json({ 
+        error: 'No hay campos para actualizar' 
+      });
+    }
+
+    // ultimo valor el ID del objeto a actualizar para el WHERE
+    valores.push(id);
+
+
+  } catch (error) {
+    
+  }
+})
+
+
 
 /*
   PUT
